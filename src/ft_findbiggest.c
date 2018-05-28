20ff0660226996497335e360284ecd750b6d626b
izcet <isaacjrhett@gmail.com>
2018-05-28 15:34:26 -0700

    [ft_findbiggest.c]

--- src/ft_findbiggest.c
+++ src/ft_findbiggest.c
@@ -6,7 +6,7 @@
 /*   By: irhett <irhett@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/21 15:38:37 by irhett            #+#    #+#             */
-/*   Updated: 2018/05/16 19:52:05 by irhett           ###   ########.fr       */
+/*   Updated: 2018/05/28 15:34:13 by irhett           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,13 @@
 
 int		ft_findbiggest(int *arr, unsigned int arr_len)
 {
-	int				biggest;
-	unsigned int	index;
+    int				biggest;
+    unsigned int	index;
 
-	index = 0;
-	if (arr && arr_len)
-	{
-		biggest = arr[0];
-		while (++index < arr_len)
-			if (arr[index] > biggest)
-				biggest = arr[index];
-	}
-	return (biggest);
+    index = 0;
+    biggest = arr[0];
+    while (++index < arr_len)
+        if (arr[index] > biggest)
+            biggest = arr[index];
+    return (biggest);
 }
